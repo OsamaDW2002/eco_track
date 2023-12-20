bab129e22428708cfac79f14125caedbe0296d5b
sohaib-arafat <sohaib.arafat@outlook.com>
2023-12-20 22:38:00 +0200

    -Removed unused imports

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const port = 6005
-const express=require('express')
-const session = require("express-session");
-const app=express()
-const router=require('./Router/user_profile')
-const {authenticateToken} = require("./user_profile/auth");
+const express = require('express')
+require("express-session");
+const app = express()
+const router = require('./Router/user_profile')
 app.use(express.json());
-app.use("/user_profile",router)
+app.use("/user_profile", router)
 
 app.listen(6005)
