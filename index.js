161ac8d1323faef8479bea6b39e6a22a4394b532
OsamaDW <s12028609@stu.najah.edu>
2023-12-23 01:29:14 +0200

    Score System

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ require("express-session");
 const app = express()
 const userProfileRoutes = require('./routers/user_profile')
 const uploadRoutes = require("./routers/upload");
+const moment = require("moment/moment");
 
 app.use(express.json());
+console.log("currentTime");
+
+const currentTime=moment();
+console.log(currentTime);
 
 app.use("/user_profile", userProfileRoutes)
 app.use("/upload", uploadRoutes)
--- methods/score_sys/daily_sign_in_score_calc.js
+++ methods/score_sys/daily_sign_in_score_calc.js
@@ -1,4 +1,55 @@
-function one_point(tookin){
+require('express');
+const con = require('../../project_connections/database_connection');
+const moment = require('moment');
 
-    return "Congratulations! You earned a one score point for daily logging";
+function EditScore(Email , Score){
+const updateScore="UPDATE Profile SET Score = Score + ?  WHERE Email = ?";
+con.query(updateScore,[Score , Email],(err) => {
+    if(err){
+        throw err;
+    }
+});
 }
+function  AddNewTime(Email , currentTime){
+    const updateScore="UPDATE Profile SET Time = ? WHERE Email = ?";
+    con.query(updateScore,[currentTime , Email],(err) => {
+        if(err){
+            throw err;
+        }
+    });
+}
+function logging_point(Email){
+    // check if there's a time before or not
+    const checkTime = "SELECT Score,logTime FROM Profile WHERE Email = ?";
+    const currentTime=moment();
+
+    con.query(checkTime,[Email.toLowerCase()],async(err,results)=>{
+        const differenceMs = Math.abs(currentTime - results[1]);
+        const differenceHours = differenceMs / (1000 * 60 * 60);
+        if(!results[1] || differenceHours > 24){
+            let Score =parseInt(results[0]);
+            Score++;
+            EditScore(Email,Score);
+            AddNewTime(Email , currentTime);
+        }
+        else {
+            console.log("Less than 24 hours have passed to obtain the point");
+        }
+    });
+}
+
+function add_points(Email , pointnum){
+    // check if there's a time before or not
+    const checkTime = "SELECT Score FROM Profile WHERE Email = ?";
+    con.query(checkTime,[Email.toLowerCase()],async(err,results)=>{
+        if(!results[0]){
+            let Score =parseInt(results[0]);
+            Score+=pointnum;
+            EditScore(Email,Score);
+        }
+        else {
+            console.log("Less than 24 hours have passed to obtain the point");
+        }
+    });
+}
+module.exports = {add_points,logging_point};
